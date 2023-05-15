c88c20578eb0df69af3e92701d0c3006040b1af5
RhogerFarias <rhoger.gabriel.12@gmail.com>
2023-05-15 16:18:07 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
-let score = 0;
-let scored = false;
+let score = 0; // Variável que armazena a pontuação inicial
+let scored = false; // Variável que verifica se a pontuação já foi adicionada na passagem
 
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
-const cloud = document.querySelector('.clouds');
-const gameover = document.querySelector('.gameover');
-const scoreDisplay = document.querySelector('.score');
-//Essaslinhas usam o método document.querySelector para encontrar elementos HTML com as classes .mario e .pipe, respectivamente, e armazená-los nas variáveis mario e pipe.
+const mario = document.querySelector('.mario'); // Seleciona o elemento HTML do Mario
+const pipe = document.querySelector('.pipe'); // Seleciona o elemento HTML do cano
+const cloud = document.querySelector('.clouds'); // Seleciona o elemento HTML da nuvem
+const gameover = document.querySelector('.gameover'); // Seleciona o elemento HTML da mensagem de Game Over
+const scoreDisplay = document.querySelector('.score'); // Seleciona o elemento HTML da pontuação
 
-const jump = () => {
+const jump = () => { // Função que faz o Mario pular
     mario.classList.add('jump');
 
     setTimeout(() => {
@@ -17,51 +16,48 @@ const jump = () => {
 
     }, 500);
 }
-//Essa função jump adiciona a classe .jump ao elemento mario, fazendo-o saltar. Em seguida, um temporizador é iniciado usando a função setTimeout, que remove a classe .jump depois de 500 milissegundos, fazendo com que o Mario volte ao chão.
 
-const loop = setInterval(() => {
+const loop = setInterval(() => { // Loop principal do jogo
     console.log('loop')
 
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');//o + converte string para number
-    const cloudPosition = +window.getComputedStyle(cloud).right.replace('px', '');
+    const pipePosition = pipe.offsetLeft; // Posição horizontal do cano
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ''); // Posição vertical do Mario
+    const cloudPosition = +window.getComputedStyle(cloud).right.replace('px', ''); // Posição horizontal da nuvem
 
-
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) { // Verifica se houve colisão entre o Mario e o cano
         cloud.style.animation = 'none';
         cloud.style.right = `${cloudPosition}px`;
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
-        
+
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
         mario.src = './images/game-over.png';
         mario.style.width = '75px';
-        mario.style.marginLeft = '50px'
-    
+        mario.style.marginLeft = '50px';
+
         clearInterval(loop);
 
-        const restartButton = document.createElement('button');
-        restartButton.textContent = 'Reiniciar';
+        const restartButton = document.createElement('button'); // Cria um novo elemento de botão
+        restartButton.textContent = 'Reiniciar'; // Define o texto do botão
         restartButton.addEventListener('click', () => {
-            location.reload();
+            location.reload(); // Recarrega a página quando o botão de reiniciar é clicado
         });
 
-        gameover.style.display = 'block';
-        gameover.appendChild(restartButton);
+        gameover.style.display = 'block'; // Exibe a mensagem de Game Over
+        gameover.appendChild(restartButton); // Adiciona o botão de reiniciar ao elemento de Game Over
 
-    }else if (marioPosition > pipePosition && !scored) {
-        score++;
-        scored = true; // atualiza a pontuação
-        scoreDisplay.textContent = `Score: ${score}`;
-      }else if (scored && marioPosition <= pipePosition) {
-        scored = false;}
+    } else if (marioPosition > pipePosition && !scored) { // Verifica se o Mario passou pela abertura do cano e ainda não pontuou
+        score++; // Incrementa a pontuação
+        scored = true; // Atualiza a pontuação para não pontuar novamente na mesma passagem
+        scoreDisplay.textContent = `Score: ${score}`; // Atualiza o elemento da pontuação com o novo valor
+    } else if (scored && marioPosition <= pipePosition) {
+        scored = false; // Permite que a pontuação seja atualizada novamente na próxima passagem
+    }
 
-},10);
-//Esta é uma função de loop que é executada a cada 10 milissegundos usando o método setInterval. Dentro do loop, a posição do cano e do Mario é verificada usando pipe.offsetLeft e window.getComputedStyle(mario).bottom, respectivamente. Se a posição do cano estiver dentro de uma certa faixa e a posição do Mario for menor que 80 pixels (indicando que ele está em cima do cano), então o jogo termina e os elementos do jogo são atualizados para mostrar a tela de "game over". O loop é interrompido usando clearInterval.
+}, 10);
 
-document.addEventListener('keydown', jump);
-//Essa linha adiciona um ouvinte de evento ao documento para escutar quando o usuário pressionar uma tecla. Quando uma tecla é pressionada, a função jump é chamada, fazendo com que o Mario pule.
+document.addEventListener('keydown', jump); // Adiciona o evento de escuta do teclado para o salto do Mario
+
